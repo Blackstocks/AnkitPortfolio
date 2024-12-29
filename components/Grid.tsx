acf8cf48ab0da9185ab25354e08e8926454f1172
blackstocks <ankitkumarsoni656.iitkha@gmail.com>
2024-12-29 23:20:05 +0530

    deployment bugs fixed

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,5 +1,9 @@
 import { gridItems } from "@/data";
-import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
+import dynamic from "next/dynamic";
+
+// Dynamically import BentoGrid and BentoGridItem to disable SSR
+const BentoGrid = dynamic(() => import("./ui/BentoGrid").then(mod => mod.BentoGrid), { ssr: false });
+const BentoGridItem = dynamic(() => import("./ui/BentoGrid").then(mod => mod.BentoGridItem), { ssr: false });
 
 const Grid = () => {
   return (
@@ -11,8 +15,6 @@ const Grid = () => {
             key={i}
             title={item.title}
             description={item.description}
-            // remove icon prop
-            // remove original classname condition
             className={item.className}
             img={item.img}
             imgClassName={item.imgClassName}
